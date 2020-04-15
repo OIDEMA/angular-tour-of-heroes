c18573ecf4630b4de261779d91d57f26ed96dbe1
“OIDEMA” <k-goda@oidema-jp.com>
2020-04-15 18:04:26 +0900

    サービスの追加

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,12 +14,20 @@ export class HeroesComponent implements OnInit {
   heroes = HEROES;
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageServive: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeros();
   }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+    this.messageServive.add(`HeroService: Selected hero id=${hero.id}`);
   }
+
+  getHeros(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
